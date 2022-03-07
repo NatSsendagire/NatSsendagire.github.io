e57c52165d9e593599519a02a7e5dd3bb5f56457
NatSsendagire <nathaniel.ssendagire@edu.keuda.fi>
2022-03-07 08:57:09 +0200

    lisättiin tervetuloa(omanimi)

---
+++ demo 6/Login.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", hasSignedIN);
+
+function hasSignedIN(){
+let signedin = 'yes';
+if(signedin === 'yes'){
+    document.getElementById('tervetuloa_teksti').textContent += 'Nathaniel!';
+}
+}
