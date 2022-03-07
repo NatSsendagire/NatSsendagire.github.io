557b0b293a0032e66c52116625f8cb8670bad807
NatSsendagire <nathaniel.ssendagire@edu.keuda.fi>
2022-03-07 10:54:28 +0200

    ijhwda

--- demo 6/Login.js
+++ demo 6/Login.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", hasSignedIN);
 function hasSignedIN(){
 let signedin = 'yes';
     if(signedin === 'yes'){
-    document.getElementById('tervetuloa_teksti').textContent += localStorage.getItem(" nimi");
+    document.getElementById('tervetuloa_teksti').textContent += localStorage.getItem("nimi");
     }
 }
 
--- demo7/Login.js
+++ demo7/Login.js
@@ -2,14 +2,11 @@ document.addEventListener("DOMContentLoaded", hasSignedIN);
 
 function hasSignedIN(){
 let signedin = 'yes';
-if(signedin === 'yes'){
-    document.getElementById('tervetuloa_teksti').textContent += " " + localStorage.getItem("name");
+    if(signedin === 'yes'){
+    document.getElementById('tervetuloa_teksti').textContent += localStorage.getItem("nimi");
+    }
 }
-}
 
-
-
-
-function signin(){
-    localStorage.setItem("name", document.getElementById("name").value);
+function kirjaudu() {
+    localStorage.setItem("nimi", document.getElementById("nimi").value);
 }
